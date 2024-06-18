1b1a48b3ce504bdcf8f891d7d57ce92cf0ad1cdb
Mflo290 <marioflo455@gmail.com>
2024-06-17 22:34:13 -0700

    Created VinList and VinInfo entities.

---
+++ app/src/main/java/com/example/vinscannerapp/entities/VinInfo.java
@@ -0,0 +1,79 @@
+package com.example.vinscannerapp.entities;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "vin_info",
+        foreignKeys = @ForeignKey(entity = VinList.class,
+                                    parentColumns = "id",
+                                    childColumns = "listId",
+                                    onDelete = ForeignKey.CASCADE))
+public class VinInfo {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String vinNumber;
+    private int listId;
+    private String year;
+    private String make;
+    private String model;
+
+
+    // Constructors, getters and setters
+    public VinInfo(String vinNumber, int listId, String year, String make, String model) {
+        this.vinNumber = vinNumber;
+        this.listId = listId;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVinNumber() {
+        return vinNumber;
+    }
+
+    public void setVinNumber(String vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ app/src/main/java/com/example/vinscannerapp/entities/VinList.java
@@ -0,0 +1,51 @@
+package com.example.vinscannerapp.entities;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "vin_lists")
+public class VinList {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private int vinCount;
+
+
+    // Constructors, getters and setters
+    public VinList(String name) {
+        this.name = name;
+        this.vinCount = 0;
+    }
+
+    @Ignore
+    public VinList(String name, int vinCount) {
+        this.name = name;
+        this.vinCount = vinCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVinCount() {
+        return vinCount;
+    }
+
+    public void setVinCount(int vinCount) {
+        this.vinCount = vinCount;
+    }
+}
