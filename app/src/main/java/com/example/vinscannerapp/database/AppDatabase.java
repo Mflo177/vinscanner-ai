91ba2b2c8b059e9b5fbda751a573dce9088b3aec
Mflo290 <marioflo455@gmail.com>
2024-06-18 20:53:56 -0700

    Created Database class

---
+++ app/src/main/java/com/example/vinscannerapp/database/AppDatabase.java
@@ -0,0 +1,34 @@
+package com.example.vinscannerapp.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.vinscannerapp.dao.VinInfoDao;
+import com.example.vinscannerapp.dao.VinListDao;
+import com.example.vinscannerapp.entities.VinInfo;
+import com.example.vinscannerapp.entities.VinList;
+
+@Database(entities = {VinList.class, VinInfo.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract VinListDao vinListDao();
+    public abstract VinInfoDao vinInfoDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    static AppDatabase getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "vin_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
