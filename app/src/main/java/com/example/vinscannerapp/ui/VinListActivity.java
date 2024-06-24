47030a83ca399647cafaabb7bc6fb5e2a2e9d1da
Mflo290 <marioflo455@gmail.com>
2024-06-23 21:59:12 -0700

    Added Menu to top Toolbar of VinListActivity.java

--- app/src/main/java/com/example/vinscannerapp/ui/VinListActivity.java
+++ app/src/main/java/com/example/vinscannerapp/ui/VinListActivity.java
@@ -1,6 +1,11 @@
 package com.example.vinscannerapp.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -27,6 +32,8 @@ public class VinListActivity extends AppCompatActivity {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_vin_list); // Set the correct layout file here
 
+        Toolbar toolbar = findViewById(R.id.id_toolbar);
+        setSupportActionBar(toolbar);
 
         recyclerView = findViewById(R.id.id_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -43,6 +50,32 @@ public class VinListActivity extends AppCompatActivity {
                 adapter.setVinInfos(vinInfos);
             }
         });
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_vin_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.id_scanIcon) {
+            // Handle scan icon click
+            Toast.makeText(this, "Scan Icon Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.id_deleteList) {
+            // Handle delete list click
+            Toast.makeText(this, "Delete List Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
+
+
+
