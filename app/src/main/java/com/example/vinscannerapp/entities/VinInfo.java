34b5f354e17dc574f144df5fc90014093a521717
Mflo290 <marioflo455@gmail.com>
2024-07-09 22:32:51 -0700

    Removed year, make, and model attributes from VinInfo class.

--- app/src/main/java/com/example/vinscannerapp/entities/VinInfo.java
+++ app/src/main/java/com/example/vinscannerapp/entities/VinInfo.java
@@ -5,32 +5,28 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.Ignore;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "vin_info",
         foreignKeys = @ForeignKey(entity = VinList.class,
                                     parentColumns = "id",
                                     childColumns = "listId",
-                                    onDelete = ForeignKey.CASCADE))
+                                    onDelete = ForeignKey.CASCADE),
+        indices = {@Index("listId")})
 public class VinInfo {
 
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String vinNumber;
     private int listId;
-    private String year;
-    private String make;
-    private String model;
 
 
     // Constructors, getters and setters
-    public VinInfo(int id, String vinNumber, int listId,@Nullable String year,@Nullable String make,@Nullable String model) {
+    public VinInfo(int id, String vinNumber, int listId) {
         this.id = id;
         this.vinNumber = vinNumber;
         this.listId = listId;
-        this.year = year;
-        this.make = make;
-        this.model = model;
     }
 
     @Ignore
@@ -63,27 +59,5 @@ public class VinInfo {
         this.listId = listId;
     }
 
-    public String getYear() {
-        return year;
-    }
 
-    public void setYear(@Nullable String year) {
-        this.year = year;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(@Nullable String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(@Nullable String model) {
-        this.model = model;
-    }
 }
